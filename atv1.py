0de1c76a9b68157060f3825f8448742f0326fcd0
Nathan Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2026-02-21 00:41:06 -0300

    Add Names and phrases

--- atv1.py
+++ atv1.py
@@ -1,22 +1,24 @@
 #atividade 1: fazer input que recebe o sálario + dependendo do cargo)junior/pleno/senior) + aumento percentual
 
+nome = input("Qual é o seu nome?")
 salario = float(input("Qual é o seu sálario?"))
 cargo = input("Qual é o seu cargo?")
 
 if cargo == "junior":
     #aumento de 10%
     aumento = salario * 0.1
-    print(f"Seu sálario é R${aumento + salario:.2f} por ser {cargo.capitalize()}")
+    print(f"Olá {nome}. Seu sálario é R${aumento + salario:.2f} por ser {cargo.capitalize()}")
 elif cargo == "pleno":
     #aumento de 15%
     aumento = salario * 0.15
-    print(f"Seu sálario é R${aumento + salario:.2f} por ser {cargo.capitalize()}")
+    print(f"Olá {nome}. Seu sálario é R${aumento + salario:.2f} por ser {cargo.capitalize()}")
 elif cargo == "senior":
     #aumento de 20%
     aumento = salario * 0.2
-    print(f"Seu sálario é R${aumento + salario:.2f} por ser {cargo.capitalize()}")
+    print(f"Olá {nome}. Seu sálario é R${aumento + salario:.2f} por ser {cargo.capitalize()}")
 else:
-    print("Por favor, digite o seu cargo (junior/pleno/senior)")
+    print(f"{nome}. Por favor, digite o seu cargo (junior/pleno/senior)")
+
 
 #atribuindo a quantidade de filhos o usuários tiver, vai ter um bônus  de R$500,00 por filho.
 filhos = input("Você tem filhos?")
@@ -24,14 +26,14 @@ if filhos == "sim":
     quantidade = input("tem quantos filhos?")
     salario_filho = aumento + salario
     if quantidade == "1":
-        print(salario_filho + 500)
+        print(f"{nome}. Seu sálario com bônus ao ter filho é de R${salario_filho + 500}")
     elif quantidade == "2":
-        print(salario_filho + 500 * 2)
+        print(f"{nome}. Seu sálario com bônus ao ter filhos é de R${salario_filho + 500 * 2}")
     elif quantidade == "3":
-        print(salario_filho + 500 * 3)
+        print(f"{nome}. Seu sálario com bônus ao ter filhos é de R${salario_filho + 500 * 3}")
     elif quantidade == "4":
-        print(salario_filho + 500 * 4)
+        print(f"{nome}. Seu sálario com bônus ao ter filhos é de R${salario_filho + 500 * 4}")
     else:
-        print(salario_filho + 2000)
+        print(f"{nome}. Seu sálario com bônus ao ter filhos é de R${salario_filho + 2000}")
 
-    
+print("Obrigado por participar dessas perguntas!")
