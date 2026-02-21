251a9deb3d82b6754c565e0f27c7890f719ee22b
Nathan Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2026-02-20 23:25:29 -0300

    Update atv1.py
    adicionando  a segunda parte, de cada filhos ter um adicional de 500 reais

--- atv1.py
+++ atv1.py
@@ -4,13 +4,34 @@ salario = float(input("Qual é o seu sálario?"))
 cargo = input("Qual é o seu cargo?")
 
 if cargo == "junior":
-    print("Você é Júnior")
     #aumento de 10%
+    aumento = salario * 0.1
+    print(f"Seu sálario é R${aumento + salario:.2f} por ser {cargo.capitalize()}")
 elif cargo == "pleno":
-    print("Você é Pleno")
     #aumento de 15%
+    aumento = salario * 0.15
+    print(f"Seu sálario é R${aumento + salario:.2f} por ser {cargo.capitalize()}")
 elif cargo == "senior":
-    print("Você é Senior")
     #aumento de 20%
+    aumento = salario * 0.2
+    print(f"Seu sálario é R${aumento + salario:.2f} por ser {cargo.capitalize()}")
 else:
     print("Por favor, digite o seu cargo (junior/pleno/senior)")
+
+#atribuindo a quantidade de filhos o usuários tiver, vai ter um bônus  de R$500,00 por filho.
+filhos = input("Você tem filhos?")
+if filhos == "sim":
+    quantidade = input("tem quantos filhos?")
+    salario_filho = aumento + salario
+    if quantidade == "1":
+        print(salario_filho + 500)
+    elif quantidade == "2":
+        print(salario_filho + 500 * 2)
+    elif quantidade == "3":
+        print(salario_filho + 500 * 3)
+    elif quantidade == "4":
+        print(salario_filho + 500 * 4)
+    else:
+        print(salario_filho + 2000)
+
+    
