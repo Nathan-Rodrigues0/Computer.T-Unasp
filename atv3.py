3cd4f595a90ecd4a719c226bf64fafe409ba4beb
Nathan Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2026-03-03 18:54:16 -0300

    Upated Commits
    add Comitis in Codes

--- atv3.py
+++ atv3.py
@@ -1,7 +1,7 @@
-tabuada = int(input("Tabuada de qual número?"))
-multiplicavel = 0
+tabuada = int(input("Tabuada de qual número?"))#recebe um numero estavel ao multiplicador
+multiplicavel = 0 #numero que ira multiplicar
 
-print(f"Tabuada do {tabuada}:")
+print(f"Tabuada do número {tabuada}:")
 while multiplicavel <= 10:
     resultado = tabuada * multiplicavel
     print(f"{tabuada} X {multiplicavel} = {resultado}")
