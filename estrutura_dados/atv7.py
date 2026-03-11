0283017522466f5d11bc30d1224b22ad86d8e1a7
Nathan Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2026-03-10 23:00:05 -0300

    atividade 7

---
+++ estrutura_dados/atv7.py
@@ -0,0 +1,12 @@
+"""number = int(input("Tabuade de: "))
+multiplicador = range(11)
+
+for n in multiplicador:
+    print(f"{number} {chr(215)} {n} = {number * n}")
+    n +=1"""
+
+number = int(input("Tabuada de: "))
+
+for multiplicador in range(11):
+    print(f"{number} {chr(215)} {multiplicador} = {number * multiplicador}")
+   
