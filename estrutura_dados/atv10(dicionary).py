ea045d65c31579445fa800bddb5632bbd0e33f51
Nathan Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2026-04-02 23:22:58 -0300

    Atividade de fazer um estoque de frutas que tenha as frutas e seus respectivos preços, sempre perguntando que precisa adiconar mais frutas/produto nesse estoque...

--- estrutura_dados/atv10(dicionary).py
+++ estrutura_dados/atv10(dicionary).py
@@ -6,3 +6,24 @@
 
 dicionary = {'number one': 1, 'fruts': "apple", 'List': [1, 2, 3, 4, 5]}
 print(dicionary)
+
+#desafio de estoque de frutas
+
+estoque = {}
+while True:
+
+    produto = input("Qual produto deseja adicionar ao estoque?")
+    valor_produto = int(input("Qual é o valor desse produto?"))
+    estoque[produto] = valor_produto
+    print(estoque)
+
+    saída = input("Quer adicionar mais produto?")
+    if saída == "sim":
+        continue
+    elif saída == "não":
+        print("Estoque finalizado")
+        print(estoque)
+        break
+    else:
+        print("Dados não reconhecido, digite novamente...")
+        continue 
