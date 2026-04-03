7b772133d29defd0a29b3270a0202af90a576b69
Nathan Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2026-04-02 22:38:35 -0300

    Add conteúdo sobre listas(tuple and dicionary)

---
+++ estrutura_dados/atv10(dicionary).py
@@ -0,0 +1,8 @@
+#lista [] = lista/array 
+#tuple () = lista inalteravel usada registro de segurança
+#dicionary {} = lista com 'chave': valor
+
+#exemple:
+
+dicionary = {'number one': 1, 'fruts': "apple", 'List': [1, 2, 3, 4, 5]}
+print(dicionary)
