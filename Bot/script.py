723259ad32c87ec453ecf45b6ad8c1ce13cf2785
Nathan Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2026-05-07 01:37:24 +0000

    Bot/update

--- Bot/script.py
+++ Bot/script.py
@@ -2,7 +2,8 @@ import requests
 import os
 from dotenv import load_dotenv
 load_dotenv(".env")
-updates = requests.get(f"https://api.telegram.org/bot{os.getenv('TELEGRAM_TOKEN')}/getUpdates")#importando  e consultando o token
+updates = requests.get(f"https://api.telegram.org/bot{os.getenv('TELEGRAM_TOKEN')}/getUpdates")#importando  e consultando o token ; get = pegar
 print(updates["result"][-1]["message"]["chat"]["id"])#retornado somente o ID 
 updates = updates.json()
 updates["result"][-1]["message"]["text"]#resultado da última mensagem do telegram
+#requests.post().json() / => na url: sendMessage = mandar mensagem
