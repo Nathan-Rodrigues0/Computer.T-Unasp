5f42cceaa1adeb3970061a33155b283b9166fc73
Nathan Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2026-05-07 01:14:43 +0000

    Testing

--- Bot/script.py
+++ Bot/script.py
@@ -3,5 +3,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv(".env")
 updates = requests.get(f"https://api.telegram.org/bot{os.getenv('TELEGRAM_TOKEN')}/getUpdates")
+print(updates)
 updates = updates.json()
 updates["result"][-1]
